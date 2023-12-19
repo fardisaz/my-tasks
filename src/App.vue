b8ecdc641205da622304c1108628f37b0323d2d9
fardis1 <fardis@automedia.de>
2023-12-19 08:39:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/Pinialogo.svg" alt="pinia logo" />
+      <h1>My Tasks</h1>
+    </header>
+  </main>
+</template>
+<script setup></script>
+
+<style scoped></style>
