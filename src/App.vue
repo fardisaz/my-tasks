1a85727a7d081245ee9a88698fd2816fe5416edb
fardis1 <fardis@automedia.de>
2023-12-19 11:01:42 +0100

    Display the tasks

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,18 @@
       <img src="./assets/Pinialogo.svg" alt="pinia logo" />
       <h1>My Tasks</h1>
     </header>
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
-<script setup></script>
+<script setup>
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
+const taskStore = useTaskStore();
+</script>
 
 <style scoped></style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  task: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
