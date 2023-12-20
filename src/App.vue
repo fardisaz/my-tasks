6c74d4ad677b25b4559c126c7ca53f1b898a223e
fardis1 <fardis@automedia.de>
2023-12-20 10:46:45 +0100

    Add, delete and update tasks from json-server

--- src/App.vue
+++ src/App.vue
@@ -16,29 +16,33 @@
       <button @click="filter = 'favs'">Favorite tasks</button>
     </nav>
     <!-- loading -->
-    <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+    <div class="loading" v-if="loading">Loading tasks...</div>
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
 
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} favs left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>You have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <button @click="taskStore.$reset">reset state</button>
   </main>
 </template>
 <script setup>
+import { storeToRefs } from "pinia";
 import { useTaskStore } from "./stores/TaskStore";
 import TaskDetails from "./components/TaskDetails.vue";
 import { ref } from "vue";
 import TaskForm from "./components/TaskForm.vue";
+
 const taskStore = useTaskStore();
+const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore);
 
 // fetch tasks
 taskStore.getTasks();
